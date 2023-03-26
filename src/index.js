505fabcdb9ef747bbf7d996c714b32e5a8cf8371
Pawel Domagala <domagalapawel11@gmail.com>
2023-03-26 01:32:55 +0100

    Add Redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
-//import ReactDOM from 'react-dom';
 import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
+import React from 'react';
+import {Provider} from 'react-redux';
+import store from './redux/store'
 import 'font-awesome/css/font-awesome.min.css';
 import './styles/normalize.scss';
 import './styles/global.scss';
 
-// ReactDOM.render(
-// 	<App />,
-// 	document.querySelector('#root')
-// );
-
 const container = document.querySelector('#root');
 const root = ReactDOMClient.createRoot(container);
 
-root.render(<App />);
+root.render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
+);
+
+/*
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store'
+import App from './App';
+import './styles/normalize.scss';
+import './styles/global.scss';
+import 'font-awesome/css/font-awesome.min.css';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.querySelector('#root')
+);
+*/
