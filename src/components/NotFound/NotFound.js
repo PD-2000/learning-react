bf15102454328391cd18c410c468e399bdb60c4d
Pawel Domagala <domagalapawel11@gmail.com>
2023-03-29 23:23:33 +0200

    Add subpages & React Router

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import PageTitle from '../PageTitle/PageTitle';
+
+const NotFound = () => {
+	return (
+		<>
+			<PageTitle>Error 404: Page not found</PageTitle>
+		</>
+	);
+};
+
+export default NotFound;
