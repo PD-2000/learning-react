8db7b6062ec6679227d25a302bd957fe88123f95
Pawel Domagala <domagalapawel11@gmail.com>
2023-04-02 17:41:44 +0200

    Add Favorite

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -1,11 +1,27 @@
 import PageTitle from '../PageTitle/PageTitle';
-//import styles from './Favorite.module.scss';
+import styles from './Favorite.module.scss';
+import {useSelector} from 'react-redux';
+import {getFavoriteCard} from '../../redux/store';
+import Card from '../Card/Card';
 
 const Favorite = () => {
+	const cards = useSelector(getFavoriteCard);
+	if(cards.length === 0){
+		return (
+			<div className={styles.favorite}>
+				<PageTitle>No favorite cards yet...</PageTitle>
+				<h3>Click on the star next to a card to add it here.</h3>
+			</div>
+		);
+	}
 	return (
 		<div>
 			<PageTitle>Favorite</PageTitle>
-			<p>Lorem ipsum</p> 
+			<div className={styles.column}>
+				<ul className={styles.cards}>
+					{cards.map(card => <Card key={card.id} title={card.title} isFavorite={card.isFavorite} />)}
+				</ul>
+			</div>
 		</div>
 	);
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -6,16 +6,17 @@ import strContains from '../utils/strContains';
 // Selectors
 export const getFilteredCards = ({cards, searchString}, columnId) =>
 	cards.filter(card => card.columnId === columnId && strContains(card.title, searchString));
-export const getAllColumns = state => state.columns;
 export const getListById = ({lists}, listId) => lists.find(list => list.id === listId);
 export const getColumnsByList = ({columns}, listId) => columns.filter(column => column.listId === listId);
 export const getAllLists = state => state.lists;
+export const getFavoriteCard = state => state.cards.filter((card) => card.isFavorite === true);
 //
 // Action Creators
 export const addColumn = payload => ({type: 'ADD_COLUMN', payload});
 export const addCard = payload => ({type: 'ADD_CARD', payload});
 export const updateSearchString = payload => ({type: 'UPDATE_SEARCHSTRING', payload});
 export const addList = payload => ({type: 'ADD_LIST', payload});
+export const toggleCardFavorite = payload => ({type: 'TOGGLE_CARD_FAVORITE', payload});
 //
 
 const reducer = (state, action) => {
@@ -28,6 +29,8 @@ const reducer = (state, action) => {
 			return {...state, searchString: action.payload};
 		case 'ADD_LIST':
 			return {...state, lists: [...state.lists, {id: shortid(), ...action.payload}]};
+		case 'TOGGLE_CARD_FAVORITE':
+			return { ...state, cards: state.cards.map(card => (card.id === action.payload) ? {...card, isFavorite: !card.isFavorite} : card)};
 		default:
 			return state;
 	}
